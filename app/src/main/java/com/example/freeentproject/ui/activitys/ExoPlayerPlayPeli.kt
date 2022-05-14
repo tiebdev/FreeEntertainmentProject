ed2503bc97f1a199216b68bbfdbaed56ad2ff740
ridoutdev <riduantieb@gmail.com>
2022-05-14 12:30:16 +0200

    fix
    Add savePos() and continuePlayer() for continue video when onStop() or onPause() comes into play

--- app/src/main/java/com/example/freeentproject/ui/activitys/ExoPlayerPlayPeli.kt
+++ app/src/main/java/com/example/freeentproject/ui/activitys/ExoPlayerPlayPeli.kt
@@ -3,9 +3,13 @@ import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
+import android.view.WindowManager
+import android.widget.Toast
 import com.example.freeentproject.databinding.ActivityExoPlayerPlayPeliBinding
 import com.google.android.exoplayer2.ExoPlayer
 import com.google.android.exoplayer2.MediaItem
+import com.google.android.exoplayer2.PlaybackException
+import com.google.android.exoplayer2.Player
 import dagger.hilt.android.AndroidEntryPoint
 
 /*
@@ -20,10 +24,14 @@ completa, la invisibilidad de la barra de control al pasar unos segundos, etc..
  */
 
 @AndroidEntryPoint
-class ExoPlayerPlayPeli : AppCompatActivity() {
+class ExoPlayerPlayPeli : AppCompatActivity(), Player.Listener {
 
     private lateinit var binding: ActivityExoPlayerPlayPeliBinding
-    private lateinit var player: ExoPlayer
+    private var player: ExoPlayer? = null
+    private var playWhenReady = true
+    private var currentItem = 0
+    private var playbackPosition: Long = 0
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -32,53 +40,98 @@ class ExoPlayerPlayPeli : AppCompatActivity() {
     }
     override fun onStart() {
         super.onStart()
-        reproducir()
+        initializePlayer()
     }
 
     override fun onPause() {
         super.onPause()
-        releasePlayer()
+        savePos()
     }
 
     override fun onStop() {
         super.onStop()
-        releasePlayer()
+        savePos()
     }
 
     override fun onResume() {
         super.onResume()
         hideSystemUi()
+        continuePlayer(savePos())
     }
 
-    private fun releasePlayer() {
-        player.stop()
-        player.release()
-        finish()
+    override fun onDestroy() {
+        super.onDestroy()
+        savePos()
     }
 
-    private fun hideSystemUi() {
-        binding.videoView.systemUiVisibility = (View.SYSTEM_UI_FLAG_LOW_PROFILE
-                or View.SYSTEM_UI_FLAG_FULLSCREEN
-                or View.SYSTEM_UI_FLAG_LAYOUT_STABLE
-                or View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY
-                or View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
-                or View.SYSTEM_UI_FLAG_HIDE_NAVIGATION)
+    private fun savePos(): Long {
+        if (player != null){
+            playbackPosition = player!!.currentPosition
+            player!!.removeListener(this)
+            player!!.release()
+            player = null
+        }
+        return playbackPosition
     }
 
-    private fun reproducir() {
+    private fun continuePlayer(playPosition: Long) {
         player = ExoPlayer.Builder(applicationContext).build()
+        player!!.addListener(this)
         binding.videoView.player = player
 
         val url = intent.getStringExtra("url")
-
         val mediaItem = item(url!!)
-        player.addMediaItem(mediaItem)
-        player.prepare()
-        player.play()
-        player.isCurrentMediaItemLive
+
+        player!!.setMediaItem(mediaItem)
+        player!!.isCurrentMediaItemLive
+        playWhenReady
+        player!!.seekTo(currentItem, playPosition)
+        player!!.prepare()
+        player!!.play()
+    }
+
+    private fun initializePlayer() {
+        player = ExoPlayer.Builder(applicationContext).build()
+        player!!.addListener(this)
+        binding.videoView.player = player
+
+        val url = intent.getStringExtra("url")
+        val mediaItem = item(url!!)
+
+        player!!.setMediaItem(mediaItem)
+        player!!.isCurrentMediaItemLive
+        playWhenReady
+        player!!.seekTo(currentItem, playbackPosition)
+        player!!.prepare()
+        player!!.play()
+    }
+
+    private fun hideSystemUi() {
+        this.window.setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
+            WindowManager.LayoutParams.FLAG_FULLSCREEN)
     }
 
     private fun item(url: String): MediaItem {
         return MediaItem.fromUri(Uri.parse(url))
+    }
+
+    override fun onPlaybackStateChanged(playbackState: Int) {
+        super.onPlaybackStateChanged(playbackState)
+
+        when(playbackState) {
+
+            Player.STATE_BUFFERING -> {
+                binding.isLoading.visibility = View.VISIBLE
+            }
+
+            Player.STATE_READY -> {
+                binding.isLoading.visibility = View.INVISIBLE
+            }
+        }
+    }
+
+    override fun onPlayerError(error: PlaybackException) {
+        super.onPlayerError(error)
+        Toast.makeText(this@ExoPlayerPlayPeli, "${error.message}", Toast.LENGTH_SHORT)
     }
 }
